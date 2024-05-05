d1b36fc21d29ea886a5572e367a1859a91f4b7b9
Alex Mwaniki <lekko254@gmail.com>
2024-05-05 20:02:02 +0000

    Project Timestamp

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,38 +18,30 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalid = (date) => date.toUTCString() === 'Invalid Date'
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-app.get("/api/:date?", function (req, res) {
-  // Extract the date parameter from the URL
-  const userDate = req.params.date;
-
-  // If the date parameter is empty, use the current time
-  let date;
-  if (!userDate) {
-    date = new Date();
-  } else {
-    // Check if the provided date is valid
-    date = new Date(userDate);
-    if (isNaN(date.getTime())) {
-      // If the date is invalid, return an error response
-      return res.status(400).json({ error: 'Invalid Date' });
-    }
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date);
+  if (isInvalid(date)) {
+    date = new Date(+req.params.date)
   }
-
-  // Format the UTC time
-  const utcTime = date.toUTCString();
-
-  // Send the response with Unix timestamp and UTC time
-  res.json({ unix: date.getTime(), utc: utcTime });
+  if (isInvalid(date)) {
+    res.json({ error: "Invalid Date" })
+    return;
+  }
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
-
-
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
